9f270c86fb2d1d597b456d2daadf429d0785336a
Lucas Lamenha <lamenhajoaolucas@gmail.com>
2025-05-19 11:08:15 -0400

    made some changes: removed sheets logging, rebuild requirements.txt for clean imports

--- ImitationSnake/main.py
+++
@@ -1,5 +0,0 @@
-from snake_game import SnakeGame
-
-if __name__ == "__main__":
-    game = SnakeGame()
-    game.run()
--- snake_game.py
+++ snake_game.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 import os
 
-from sheets_logger import GoogleSheetsLogger
+
 from config import *
 
 # Calculate dimensions
